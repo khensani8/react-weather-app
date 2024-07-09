84114473a62af263b7d4fd337992ec519ebf36b8
Khensani <129982152+khensani8@users.noreply.github.com>
2024-07-10 00:34:19 +0200

    Added the Github repo link

--- src/App.js
+++ src/App.js
@@ -3,7 +3,15 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-     Weather App
+      Weather App
+      <footer>
+        This project was coded by Khensani Ngobeni and is {""}
+        <a
+          href="https://github.com/khensani8/react-weather-app"
+          target="_blank"
+          rel="noreferrer"> open-sourced on Github
+        </a>
+      </footer>
     </div>
   );
 }
