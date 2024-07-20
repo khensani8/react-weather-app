9988592b79138241a668c93b93d83d0e3dfdd6d2
Khensani <129982152+khensani8@users.noreply.github.com>
2024-07-20 10:28:24 +0200

    added basic styling

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
+import Weather from "./Weather.js";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <Weather />
       <footer>
         This project was coded by Khensani Ngobeni and is {""}
         <a
           href="https://github.com/khensani8/react-weather-app"
           target="_blank"
-          rel="noreferrer"> open-sourced on Github
+          rel="noreferrer">
+          {" "}
+          open-sourced on Github
         </a>
       </footer>
     </div>
---
+++ src/Weather.js
@@ -0,0 +1,52 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city"
+              className="form-control"
+              autoFocus="on"
+            />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-3">
+            <input type="submit" value="Search" className="btn btn-primary" />
+          </div>
+        </div>
+      </form>
+
+      <h1>Johannesburg</h1>
+
+      <ul>
+        <li>Thursday</li>
+        <li>Clear Skies</li>
+      </ul>
+      <div className="row mt-3">
+        <div className="col-6">
+          <div className="clearfix">
+            <img
+              src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAALEgAACxIB0t1+/AAAAQBJREFUaN7t2csNwyAMBmBGYYSMwhgdgxEYjRW6ARu4HNyqB0CKednElf5b2/hLSALGAICRHKMABSjgUMDdD7xfLifkxByoJOJ33O3/nwHIhVgsKDWKriXhb+0WQD6wJxZegvhlADzrcUDhpeFlpwLyAa5BZ711Na4pgAXFNxFdABw2K4r/R9iRgLiw+N89MQSATxvYFN8F2DB0qkOJCggbi/8m9AASA0AiAXBuA0ziKIDACBAogMgIECkAYBUFKEABzwOIf4yKf5HJnkqIn8wxmk775y5oxC8pj1jUH9FWEd/YOqK1eERz94j2euFqUCF7NzjYbzHpLqUCFKCAJfkAq7RimK7qUtAAAAAASUVORK5CYII="
+              alt="Clear Skies"
+            />
+            <span className="temperature">6</span>
+            <span className="unit">°C</span>
+          </div>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 0%</li>
+            <li>Humidity: 29%</li>
+            <li>Wind: 10km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
