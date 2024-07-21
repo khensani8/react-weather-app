dd2d57125340465dbc76365c0cf3f1d79159a38f
Khensani <129982152+khensani8@users.noreply.github.com>
2024-07-21 11:38:33 +0200

    added date + time to the application

--- src/Weather.js
+++ src/Weather.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import FormattedDate from "./FormattedDate";
 import axios from "axios";
 import "./Weather.css";
 
@@ -14,7 +15,7 @@ export default function Weather(props) {
       wind: response.data.wind.speed,
       description: response.data.weather[0].description,
       city: response.data.name,
-      date: "Sunday 10:00",
+      date: new Date(response.data.dt * 1000),
       humidity: response.data.main.humidity,
     });
   }
@@ -42,7 +43,9 @@ export default function Weather(props) {
         <h1>{weatherData.city}</h1>
 
         <ul>
-          <li>{weatherData.date}</li>
+          <li>
+            <FormattedDate date={weatherData.date} />
+          </li>
           <li className="text-capitalize">{weatherData.description}</li>
         </ul>
         <div className="row mt-3">
