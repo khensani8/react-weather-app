46de2d177006f6295aee3324c981f36a59600d79
Khensani <129982152+khensani8@users.noreply.github.com>
2024-07-22 18:15:52 +0200

    created weathericon component

--- src/Weather.js
+++ src/Weather.js
@@ -15,8 +15,7 @@ export default function Weather(props) {
       humidity: response.data.main.humidity,
       date: new Date(response.data.dt * 1000),
       description: response.data.weather[0].description,
-      iconUrl: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
-      wind: response.data.wind.speed,
+      icon: response.data.weather[0].icon,
       city: response.data.name,
     });
   }
